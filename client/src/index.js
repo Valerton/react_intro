8de3ef6ce0f623e7b69cecf0ee7d4ff42f4fffa9
Valerton <valera14644@gmail.com>
2021-05-29 17:04:12 +0300

    Initial commit.

---
+++ client/src/index.js
@@ -0,0 +1,13 @@
+//other import
+import React from 'react'
+import ReactDOM from 'react-dom'
+//my components
+import {App} from './App'
+
+ReactDOM.render(
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>,
+    document.getElementById('root')
+)
+
