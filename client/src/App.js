f15c4d889a5d10715b8b5189b03513c7193b07ca
Valerton <valera14644@gmail.com>
2021-06-01 16:03:13 +0300

    Success test.

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,19 @@
+//other import
+import {useState, lazy, Suspense} from 'react'
+//my components
+const Modal = lazy(() => import('./components/Modal/Modal'))
+
 export const App = () => {
+
+    const [isOpen, setIsOpen] = useState(false)
+
     return (
-        <div>
-            home
+        <div className='lala'>
+            <button onClick={() => setIsOpen(!isOpen)}>toggle modal</button>
+            <Suspense fallback={<div>Loading...</div>}>
+                {isOpen && <Modal/>}
+            </Suspense>
+            hello world
         </div>
     )
 }
